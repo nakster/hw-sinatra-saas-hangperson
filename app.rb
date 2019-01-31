7ca356b207bff29cc95b54f8d847dad52dc83d75
nakster <g00332403@gmit.ie>
2019-01-31 19:38:44 +0000

    fixing problem

--- app.rb
+++ app.rb
@@ -66,7 +66,9 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win then redirect '/win'
+    elsif @game.check_win_or_lose == :lose then redirect '/lose'
+    else erb :show end# You may change/remove this line
   end
   
   get '/win' do
